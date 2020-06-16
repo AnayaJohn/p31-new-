42211c525f9b34e7d31ea1e4deea9c668a71b43b
AnayaJohn <64412959+AnayaJohn@users.noreply.github.com>
2020-06-16 19:45:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+
+var plinkos=[];
+var divisions=[];
+var particles=[];
+var divisionHeight=300;
+
+function setup() {
+  createCanvas(480,800);
+  engine = Engine.create();
+    world = engine.world;
+  //createSprite(400, 200, 50, 50);
+  ground=new Ground(240,800,480,10);
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+
+  ground.display();
+  
+for(var k=0; k<=width; k=k+80 ){
+  divisions.push(new Division(k, height-divisionHeight/2,10, divisionHeight));
+}
+
+if(frameCount%60===0){
+  particles.push(new Particle(random(width/2-10,width/2+10),10,10))
+}
+
+for(var j=40; j<=width; j=j+50){
+  plinkos.push(new Plinko(j,75));
+}
+
+for(var j=15; j<=width; j=j+50){
+  plinkos.push(new Plinko(j,175));
+  //plinkos[j].display();
+}
+
+for(var j=0; j<particles.length; j++){
+  //plinkos.push(new Plinko(j,75));
+particles[j].display();
+}
+
+for(var k=0; k<divisions.length; k++){
+  //plinkos.push(new Plinko(j,75));
+divisions[k].display();
+}
+
+  drawSprites();
+}
